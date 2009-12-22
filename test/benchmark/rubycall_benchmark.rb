85a1b1cd823ee89dbb17225018ccfb1caec8c15a
Simon Chiang <simon.chiang@pinnacol.com>
2009-12-22 11:40:37 -0700

    added an api benchmark test

--- test/benchmark/rubycall_benchmark.rb
+++ test/benchmark/rubycall_benchmark.rb
@@ -6,7 +6,7 @@ class RubycallBenchmark < Test::Unit::TestCase
   acts_as_subset_test
   
   def test_rubycall_speed
-    benchmark_test do |x|
+    benchmark_test(20) do |x|
       n = 10
       block = lambda { }
       block_id = block.object_id.to_s
@@ -16,8 +16,16 @@ class RubycallBenchmark < Test::Unit::TestCase
         (n * 1000).times { block.call }
       end
       
-      x.report("#{n}k rubycall") do
+      x.report("#{n}k env.call") do
         (n * 1000).times { env.call("ruby-call", block_id) }
+      end
+      
+      x.report("#{n}k EnvFunctionCall") do
+        ptr = env.pointer
+        obj = Clips::Api::DataObject.new
+        api = Clips::Api::Environment
+        
+        (n * 1000).times { api::EnvFunctionCall(ptr, "ruby-call", block_id, obj) }
       end
     end
   end
