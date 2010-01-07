22780836be1d20c1da882892361794a5e27f225e
Simon Chiang <simon.chiang@pinnacol.com>
2010-01-07 08:08:31 -0700

    reworked multifield with each + Enumerable

--- lib/clipr/api/struct/multifield.rb
+++ lib/clipr/api/struct/multifield.rb
@@ -15,51 +15,35 @@ module Clipr
       #      struct field theFields[1];
       #     };
       #
+      # === Implementation Notes
+      #
+      # CLIPS uses a trick with multifields where theFields array is declared
+      # as being of length 1, but in fact holds an arbitrary number of Fields.
+      # This works in C because arrays do not do range checking, but it breaks
+      # in FFI where range checking does occur.
+      #
+      # One solution is to set theFields extent to a large number, ie
+      # something like this:
+      #
+      #   layout ... :theFields, [Field, 1000]
+      #
+      #   # breaks for i > 1000
+      #   def each
+      #     @begin.upto(@end) do |i|
+      #       yield self[:theFields][i]
+      #     end
+      #   end
+      #
+      # A slower but more robust approach is what is done here, where each
+      # manually increments the field pointer.
       class Multifield < FFI::Struct
         class << self
           def contents(obj)
-            fields(obj) {|field| field.contents }
+            new(obj).collect {|field| field.contents }
           end
           
           def value(obj)
-            fields(obj) {|field| field.value }
-          end
-          
-          private
-          
-          # Yields each field in the object to the block.
-          #
-          # === Implementation Notes
-          #
-          # CLIPS uses a trick with multifields where theFields array is
-          # declared as being of length 1, but hold an arbitrary number of
-          # Fields.  This works in C because there is no range checking on an
-          # array, however it breaks in FFI where range checking does occur.
-          #
-          # One solution is to set theFields extent to a large number, ie:
-          #
-          #   layout :theFields, [Field, 1000]
-          #
-          #   # breaks for i > 1000
-          #   def fields(obj)
-          #     fields = new(obj[:value])[:theFields]
-          #     obj[:begin].upto(obj[:end]) do |i|
-          #       yield(fields[i])
-          #     end
-          #   end
-          #
-          # A slower but more robust approach is what is done here, where the
-          # iterator manually increments the field pointer.
-          def fields(obj) # :nodoc:
-            offest = new(obj[:value])[:theFields].pointer.address
-            size = Field.size
-            
-            values = []
-            obj[:begin].upto(obj[:end]) do |i|
-              pointer = FFI::Pointer.new(offest + i * size)
-              values << yield(Field.new(pointer))
-            end
-            values
+            new(obj).collect {|field| field.value }
           end
         end
         
@@ -68,6 +52,24 @@ module Clipr
                :multifieldLength, :long,
                :next, :pointer,
                :theFields, Field
+        
+        include Enumerable
+        FIELD_SIZE = Field.size
+        
+        def initialize(obj)
+          super(obj[:value])
+          
+          @begin = obj[:begin]
+          @end = obj[:end]
+          @offest = self[:theFields].pointer.address
+        end
+        
+        def each
+          @begin.upto(@end) do |i|
+            ptr = FFI::Pointer.new(@offest + i * FIELD_SIZE)
+            yield Field.new(ptr)
+          end
+        end
       end
     end
   end
